337d979e3e86b6d901e79014aa9dc1f2b7248e32
edu-se <edu.se.dev@proton.me>
2026-09-10 12:39:48 +0000

    todas as entradadas de dado adicionadas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,10 +7,15 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   int estadob, total;
-  char codeA [3] = '01', codeB [3] = '02', codeC [3] = '03', codeD [3] = '04', codeE [3] = '05', codeF [3] = '06', codeG [3] = '07', codeH [3] = '08';
-  char cidadA [20], cidadB [20], cidadC [20], cidadD [20], cidadE [20], cidadF [20], cidadG [20], cidadH [20];
-  char cartaA, cartaB, cartaC, cartaD, cartaE, cartaF, cartaG, cartaH;
   char estadA, estadB, estadC, estadD, estadE, estadF, estadG, estadH;
+  char codeA [3] = "01", codeB [3] = "02", codeC [3] = "03", codeD [3] = "04", codeE [3] = "05", codeF [3] = "06", codeG [3] = "07", codeH [3] = "08";
+  char cidadA [20], cidadB [20], cidadC [20], cidadD [20], cidadE [20], cidadF [20], cidadG [20], cidadH [20];
+  int  populA, populB, populC, populD, populE, populF, populG, populH;   
+  float km2A, km2B, km2C, km2D, km2E, km2F, km2G, km2H;
+  float pibA, pibB, pibC, pibD, pibE, pibF, pibG, pibH;
+  int turistA, turistB, turistC, turistD, turistE, turistF, turistG, turistH;
+  char cartaA, cartaB, cartaC, cartaD, cartaE, cartaF, cartaG, cartaH;
+
 
 
   // Área para entrada de dados
@@ -18,6 +23,14 @@ int main() {
   scanf ("%s", &estadA);
   printf("Digite o nome da cidade: \n");
   scanf("%s", &cidadA);
+  printf("Digite a quantidade de habitantes: \n");
+  scanf("%d", &populA);
+  printf("Digite a área em km²: \n");
+  scanf("%f", &km2A);
+  printf("Digite o PIB: \n");
+  scanf("%f", &pibA);
+  printf("Digite a quantidade de pontos turisticos: \n");
+  scanf("%d", &turistA);
   // Área para exibição dos dados da cidade
 
 return 0;
