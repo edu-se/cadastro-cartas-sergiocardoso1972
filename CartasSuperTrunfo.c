d555738e62456bc32a122472383919c25cd8f1c6
edu-se <edu.se.dev@proton.me>
2026-09-10 02:03:09 +0000

    entrada de estado e cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,13 +6,19 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  int n, total;
+  int estadob, total;
+  char codeA [3] = '01', codeB [3] = '02', codeC [3] = '03', codeD [3] = '04', codeE [3] = '05', codeF [3] = '06', codeG [3] = '07', codeH [3] = '08';
+  char cidadA [20], cidadB [20], cidadC [20], cidadD [20], cidadE [20], cidadF [20], cidadG [20], cidadH [20];
+  char cartaA, cartaB, cartaC, cartaD, cartaE, cartaF, cartaG, cartaH;
+  char estadA, estadB, estadC, estadD, estadE, estadF, estadG, estadH;
+
 
   // Área para entrada de dados
-
+  printf("Digite o estado: \n");
+  scanf ("%s", &estadA);
+  printf("Digite o nome da cidade: \n");
+  scanf("%s", &cidadA);
   // Área para exibição dos dados da cidade
-  printf("Olá Mundo, no Codespace!\n");
-  printf("Exemplo de edição de código!\n");
 
 return 0;
 } 
